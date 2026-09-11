8f098780bf71f1a31676403ef31325be14ffa48b
LindenC1018 <64608388+LindenC1018@users.noreply.github.com>
2026-09-10 22:52:44 -0400

    Update server.js

--- API_Project_2/server.js
+++ API_Project_2/server.js
@@ -4,7 +4,7 @@ import axios from "axios";
 
 const app = express();
 const port = 4001;
-const API_URL = "http://localhost:4000";
+const API_URL = "http://localhost:4002";
 
 app.use(express.static("public"));
 
